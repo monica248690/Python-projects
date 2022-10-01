c88b6d6859702d45d5ad529f0a27d54b01d742c4
monica248690 <114804443+monica248690@users.noreply.github.com>
2022-10-01 14:42:52 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"summer in Nagaland is so {adj}! It makes me so happy all the time because \
+I am love to {verb1}. Stay cold and {verb2} like you are {famous_person}!"
+
+print(madlib)
